c4ebf569b0541708f408d07596a3bf1d1594b70f
suryanshranjan <suryanshranjan21@gmail.com>
2025-05-12 23:02:24 +0530

    Add built JAR files

--- Hack-Overflow Backend/src/main/java/com/example/Hack/Overflow/HackOverflowApplication.java
+++ Hack-Overflow Backend/src/main/java/com/example/Hack/Overflow/HackOverflowApplication.java
@@ -2,15 +2,14 @@ package com.example.Hack.Overflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(scanBasePackages = "com.example.Hack.Overflow")
 
 public class HackOverflowApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		SpringApplication.run(HackOverflowApplication.class, args);
-	}
+        SpringApplication.run(HackOverflowApplication.class, args);
+    }
 
 }
